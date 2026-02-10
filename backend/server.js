0abf034b54b6ddbed2333ab22aec2c5d53a54da9
Dominic Wooldridge <dominicw@chalmers.se>
2026-02-10 20:38:10 +0100

    Project Setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
