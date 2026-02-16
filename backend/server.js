ad68056ad4c7cfb1eadceb7fb67cdecb59b1a969
Dominic Wooldridge <dominicw@chalmers.se>
2026-02-16 14:24:39 +0100

    Front end setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -14,6 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "5mb" })); // parse JSON req.body. Have a limit for the req.body for large images
 app.use(cookieParser());
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import Layout from "./components/layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SignUpPage from "./pages/auth/SignUpPage";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
-  return <Layout>Hello World</Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
